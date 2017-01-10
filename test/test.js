3a89f67ef634ad500e9dc52857652534a5bcfc04
jupiterli <jupiterli0806@gmail.com>
2017-01-10 13:14:29 +1100

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,14 @@
+/**
+ * Created by jupiterli on 10/01/2017.
+ *
+ * jasmine unit test file
+ */
+
+/**
+ * simple test
+ * */
+describe("True", function () {
+    it("Should be ture", function() {
+        expect(true).toBeTruthy();
+    });
+});
